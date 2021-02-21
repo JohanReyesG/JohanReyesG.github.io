dfaa47fb35d2dbcdefa37ba739a54d4201c489ff
JohanReyesG <reyesjohan665@gmail.com>
2021-02-21 13:54:31 -0500

    deploy

---
+++ precache-manifest.7dc0ca8e2d4d05d5579573f80c02828e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd2de13954f09012da5",
+    "url": "/css/app.e6f53384.css"
+  },
+  {
+    "revision": "584bbd2b0d026ae846acb970146a578e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1587ededd149e90abb15",
+    "url": "/js/about.b09639e2.js"
+  },
+  {
+    "revision": "dfd2de13954f09012da5",
+    "url": "/js/app.402eef7b.js"
+  },
+  {
+    "revision": "f684cf91e0fc111ebd81",
+    "url": "/js/chunk-vendors.84fff1c1.js"
+  },
+  {
+    "revision": "96db0612ade343fc9d9d146e5e5a4784",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

aa40999c02e1ca0fad5699195647b958de7d9ef9
JohanReyesG <reyesjohan665@gmail.com>
2022-05-06 16:12:42 -0500

    deploy

---
+++ precache-manifest.d5815302435d9afa6ccc57a5d80001a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596a8fbcc5d6b96af351",
+    "url": "/css/app.e6f53384.css"
+  },
+  {
+    "revision": "df1adb70f1b0e06a34653b917c3c95b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1587ededd149e90abb15",
+    "url": "/js/about.b09639e2.js"
+  },
+  {
+    "revision": "596a8fbcc5d6b96af351",
+    "url": "/js/app.675a6939.js"
+  },
+  {
+    "revision": "f684cf91e0fc111ebd81",
+    "url": "/js/chunk-vendors.84fff1c1.js"
+  },
+  {
+    "revision": "96db0612ade343fc9d9d146e5e5a4784",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
